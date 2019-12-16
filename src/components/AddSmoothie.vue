e7a12bcd7f13ea11999a2260e5d60a1a461c2f33
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2019-12-17 01:34:04 +0600

    connect with firebase database

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="add-smoothie container mt-4">
+        <h3 class="center-align indigo-text">Add New Smoothie Recipe</h3>
+        <form @submit.prevent="AddSmoothie">
+            <div class="field title">
+                <label for="title">Smoothie Title</label>
+                <input type="text" name="title" v-model="title">
+            </div>
+            <div class="field add-ingredient">
+                <label for="add-ingredient">Add an ingredient</label>
+                <input type="text" name="add-ingredient">
+            </div>
+            <div class="field center-align">
+                <button class="btn green">Add Smoothie</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddSmoothie',
+    data(){
+        return{
+            title: null
+        }
+    },
+    methods: {
+        AddSmoothie(){
+            console.log(this.title)
+        }
+    } 
+}
+</script>
+
+<style>
+.add-smoothie{
+    padding: 20px;
+    max-width: 600px;
+}
+.add-smoothie h3{
+    font-size:2.5em;
+}
+.add-smoothie .field{
+    margin: 20px auto;
+}
+</style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -17,25 +17,42 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'Index',
   data () {
     return {
-      smoothies: [
-        { title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'milk'], id: '1'},
-        { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: '2'}
-      ]
+      smoothies: []
       
     }
   },
   methods: {
     deleteSmoothie(id){
-      this.smoothies = this.smoothies.filter(smoothie =>{
+      //delete doc from firestore database
+      db.collection('smoothies').doc(id).delete()
+      .then(() => {
+        this.smoothies = this.smoothies.filter(smoothie =>{
         return smoothie.id != id
+      })
+      })
+      
+    }
+  },
+
+    created(){
+      //fetch data from firestore
+      db.collection('smoothies').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          let smoothie = doc.data()
+          smoothie.id = doc.id
+          this.smoothies.push(smoothie)
+        })
       })
     }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
